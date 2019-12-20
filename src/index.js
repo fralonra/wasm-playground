2c5c4a804305d36fbc30bfe233df6de5edcf29c2
fralonra <fralonra@aliyun.com>
2019-12-20 18:25:10 +0800

    add dotdot

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const path = require('path')
 const Felid = require('felid')
 
@@ -7,17 +8,36 @@ app.plugin(require('felid-serve'), {
   root: path.resolve(__dirname, '../static'),
 })
 app.plugin(require('felid-handlebars'), {
-  root: path.resolve(__dirname, 'views')
+  root: path.resolve(__dirname, 'views'),
+  onInit (hbs) {
+    const partialPath = path.resolve(__dirname, 'views/partials')
+    const files = fs.readdirSync(partialPath).filter(f => f.endsWith('.hbs'))
+    files.forEach(file => {
+      hbs.registerPartial(file.split('.hbs')[0], fs.readFileSync(path.resolve(partialPath, file), 'utf8'))
+    })
+  }
 })
 
-app.get('/public/:file', (req, res) => {
-  res.serve(req.params.file)
+app.get('/public/*', (req, res) => {
+  res.serve(req.params['*'])
 })
 
 app.get('/', (req, res) => {
   res.render('index.hbs', {
     logoPath: 'public/logo.svg',
     title: 'Felid Now'
+  })
+})
+
+app.get('/wasm/:path', (req, res) => {
+  res.render('wasm.hbs', {
+    path: req.params.path
+  })
+})
+
+app.get('/:path', (req, res) => {
+  res.render('path.hbs', {
+    path: req.params.path
   })
 })
 
